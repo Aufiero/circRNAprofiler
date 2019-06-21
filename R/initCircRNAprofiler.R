55a9a70a2d6690921ad80ad670816ca643094cc0
LAPTOP-LS60IEJD\simoa <simo.aufiero@gmail.com>
2019-06-21 16:03:11 +0200

    used file.path() insted of paste. corrected typo: Tamplate to Template

--- R/initCircRNAprofiler.R
+++ R/initCircRNAprofiler.R
@@ -50,28 +50,28 @@ initCircRNAprofiler <-
 
         # Create a folder for each prediction tool
         for (i in seq_along(detectionTools)) {
-            dir.create(paste(projectFolderName, detectionTools[i], sep = "/"))
+            dir.create(file.path(projectFolderName, detectionTools[i]))
         }
 
         # Create a template for the experiment.txt
-        getExperimentTamplate(projectFolderName)
+        getExperimentTemplate(projectFolderName)
 
-        # Create a tamplate for the motifs.txt
-        getMotifsTamplate(projectFolderName)
+        # Create a template for the motifs.txt
+        getMotifsTemplate(projectFolderName)
 
-        # Create a tamplate for the traits.txt
-        getTraitsTamplate(projectFolderName)
+        # Create a template for the traits.txt
+        getTraitsTemplate(projectFolderName)
 
 
-        # Create a tamplate for the miRs.txt
-        getMiRsTamplate(projectFolderName)
+        # Create a template for the miRs.txt
+        getMiRsTemplate(projectFolderName)
 
-        # Create a tamplate for the transcripts.txt
-        getTranscriptsTamplate(projectFolderName)
+        # Create a template for the transcripts.txt
+        getTranscriptsTemplate(projectFolderName)
     }
 
 # Experiment template
-getExperimentTamplate <- function(projectFolderName) {
+getExperimentTemplate <- function(projectFolderName) {
     experiment <-
         data.frame(matrix(nrow = 0, ncol = 3))
     colnames(experiment) <-
@@ -79,7 +79,7 @@ getExperimentTamplate <- function(projectFolderName) {
     file.create
     utils::write.table(
         experiment,
-        paste0(projectFolderName, "/experiment.txt"),
+        file.path(projectFolderName, "experiment.txt"),
         sep = "\t",
         quote = FALSE,
         row.names = FALSE
@@ -87,14 +87,14 @@ getExperimentTamplate <- function(projectFolderName) {
 }
 
 
-# Create a tamplate for the motifs.txt
-getMotifsTamplate <- function(projectFolderName) {
+# Create a template for the motifs.txt
+getMotifsTemplate <- function(projectFolderName) {
     motifs <-
         data.frame(matrix(nrow = 0, ncol = 3))
     colnames(motifs) <- c("id", "motif", "length")
     utils::write.table(
         motifs,
-        paste0(projectFolderName, "/motifs.txt"),
+        file.path(projectFolderName, "motifs.txt"),
         sep = "\t",
         quote = FALSE,
         row.names = FALSE
@@ -102,42 +102,42 @@ getMotifsTamplate <- function(projectFolderName) {
 
 }
 
-# Create a tamplate for the traits.txt
-getTraitsTamplate <- function(projectFolderName) {
+# Create a template for the traits.txt
+getTraitsTemplate <- function(projectFolderName) {
     traits <-
         data.frame(matrix(nrow = 0, ncol = 1))
     colnames(traits) <- c("id")
     utils::write.table(
         traits,
-        paste0(projectFolderName, "/traits.txt"),
+        file.path(projectFolderName, "traits.txt"),
         sep = "\t",
         quote = FALSE,
         row.names = FALSE
     )
 }
 
-# Create a tamplate for the miRs.txt
-getMiRsTamplate <- function(projectFolderName) {
+# Create a template for the miRs.txt
+getMiRsTemplate <- function(projectFolderName) {
     miRs <-
         data.frame(matrix(nrow = 0, ncol = 1))
     colnames(miRs) <- c("id")
     utils::write.table(
         miRs,
-        paste0(projectFolderName, "/miRs.txt"),
+        file.path(projectFolderName, "miRs.txt"),
         sep = "\t",
         quote = FALSE,
         row.names = FALSE
     )
 }
 
-# Create a tamplate for the transcripts.txt
-getTranscriptsTamplate <- function(projectFolderName) {
+# Create a template for the transcripts.txt
+getTranscriptsTemplate <- function(projectFolderName) {
     transcripts <-
         data.frame(matrix(nrow = 0, ncol = 1))
     colnames(transcripts) <- c("id")
     utils::write.table(
         transcripts,
-        paste0(projectFolderName, "/transcripts.txt"),
+        file.path(projectFolderName, "transcripts.txt"),
         sep = "\t",
         quote = FALSE,
         row.names = FALSE
